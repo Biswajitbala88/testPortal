7ab72a4eeca20587961c1340e3487ceae2e798f1
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-14 18:56:50 +0530

    added some comments

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::controller(SearchController::class)->group(function(){
     Route::get('autocomplete', 'autocomplete')->name('autocomplete');
 });
 
+// Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider and all of them will
+// | be assigned to the "web" middleware group. Make something great!
+
